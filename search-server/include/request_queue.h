5b1067d9099b80e61051c44f6ce7dbb258e4ed21
dynnik <110823273+dynnik@users.noreply.github.com>
2022-10-16 09:36:26 +0300

    Update request_queue.h

--- search-server/include/request_queue.h
+++ search-server/include/request_queue.h
@@ -9,16 +9,20 @@
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
+    
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
+    
     int GetNoResultRequests() const;
+    
 private:
     struct QueryResult {
         std::string raw_query;
         std::vector<Document> results;
     };
+    
     std::deque<QueryResult> requests_;
     const static int sec_in_day_ = 1440;
     const SearchServer *server;
