dde1ea2a5fee69d0806e176404c924d01c1a957d
dynnik <110823273+dynnik@users.noreply.github.com>
2023-02-03 21:09:22 +0300

    Update document.h

--- search-server/include/document.h
+++ search-server/include/document.h
@@ -1,23 +1,10 @@
 #pragma once
-
 #include <iostream>
+#include <vector>
 
-struct Document {
-    Document() = default;
+using namespace std::string_literals;
 
-    Document(int id, double relevance, int rating)
-        : id(id)
-        , relevance(relevance)
-        , rating(rating) {
-    }
-
-    int id = 0;
-    double relevance = 0.0;
-    int rating = 0;
-    
-    friend std::ostream& operator<<(std::ostream& out, const Document& document);
-    
-};
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
 enum class DocumentStatus {
     ACTUAL,
@@ -25,3 +12,16 @@ enum class DocumentStatus {
     BANNED,
     REMOVED,
 };
+
+struct Document {
+    Document();
+    Document(int id, double relevance, int rating);
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, const Document& document);
+void PrintDocument(const Document& document);
+void PrintMatchDocumentResult(int document_id, std::vector<std::string_view> words, DocumentStatus status);
