f673e43db6eb325635745f5e1848b2cf4c3941b4
dynnik <110823273+dynnik@users.noreply.github.com>
2023-02-03 21:20:50 +0300

    Update string_processing.cpp

--- search-server/src/string_processing.cpp
+++ search-server/src/string_processing.cpp
@@ -1,23 +1,16 @@
 #include "string_processing.h"
-
-using namespace std;
-std::vector<string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            word += c;
+ 
+std::vector<std::string_view> SplitIntoWords(std::string_view str) {
+   std::vector<std::string_view> result;
+    const int64_t pos_end = str.npos;
+    while (true) {
+        int64_t space = str.find(' ');
+        result.push_back(space == pos_end ? str : str.substr(0, space));
+        if (space == pos_end) {
+            break;
+        } else {
+            str.remove_prefix(space + 1);
         }
     }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
+    return result;
 }
