69fff7911651e8fcf7830c370746dbde469eca82
dynnik <110823273+dynnik@users.noreply.github.com>
2023-02-03 21:11:39 +0300

    Update request_queue.h

--- search-server/include/request_queue.h
+++ search-server/include/request_queue.h
@@ -1,41 +1,45 @@
 #pragma once
-
-#include <deque>
+ 
 #include <string>
 #include <vector>
-
+#include <deque>
+ 
 #include "search_server.h"
-
+ 
 class RequestQueue {
 public:
+ 
     explicit RequestQueue(const SearchServer& search_server);
-    
+ 
     template <typename DocumentPredicate>
-    int AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
-    int AddFindRequest(const std::string& raw_query, DocumentStatus status);
-    int AddFindRequest(const std::string& raw_query);
-    
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+ 
     int GetNoResultRequests() const;
-    
+ 
 private:
-    struct QueryResult {
-        int results;
-    };
-    
-    std::deque<QueryResult> requests_;
+ 
+    std::deque<bool> requests_;
     const static int sec_in_day_ = 1440;
-    const SearchServer *server;
-    int number_of_empty_docs_;
-
-    void AddResult(const QueryResult &result);
-};
-
-
+    const SearchServer& search_server_;
+    int seconds = 0;
+}; 
+ 
 template <typename DocumentPredicate>
-int RequestQueue::AddFindRequest(const std::string& raw_query,
-                                                   DocumentPredicate document_predicate) {
-    QueryResult result;
-    result.results = server->FindTopDocuments(raw_query, document_predicate).size();
-    AddResult(result);
-    return result.results;
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+ 
+    std::vector<Document> docs = search_server_.FindTopDocuments(raw_query, document_predicate);
+ 
+    seconds++;
+ 
+    while (seconds > sec_in_day_) {
+        requests_.pop_front();
+        seconds--;
+    }
+ 
+    docs.empty() ? requests_.push_back(0) : requests_.push_back(1);
+ 
+    return docs;
+ 
 }
